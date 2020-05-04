c310d298f9e82950e7a631d08f902ec8a24c3181
saro2808 <saro.harutyunyan@gmail.com>
2020-05-04 14:54:53 +0300

    Refreshed encryptor.py; added vernam cipher, cyrillic letters, punctuation and spaces encryption

--- encryptor.py
+++ encryptor.py
@@ -1,4 +1,6 @@
 import argparse
+import random
+import string
 
 
 parser = argparse.ArgumentParser()
@@ -12,8 +14,51 @@ parser.add_argument("--model-file")
 args = parser.parse_args()
 
 
+baudot_code = {
+    'a': 4,
+    'b': 9,
+    'c': 13,
+    'd': 15,
+    'e': 2,
+    'f': 11,
+    'g': 10,
+    'h': 14,
+    'i': 3,
+    'j': 12,
+    'k': 28,
+    'l': 30,
+    'm': 26,
+    'n': 27,
+    'o': 7,
+    'p': 31,
+    'q': 29,
+    'r': 25,
+    's': 17,
+    't': 21,
+    'u': 5,
+    'v': 23,
+    'w': 19,
+    'x': 18,
+    'y': 1,
+    'z': 22,
+    '0': 0,
+    '5': 6,
+    '4': 8,
+    '3': 16,
+    '2': 20,
+    '1': 24
+}
+
+space_punct_list = string.punctuation + ' ' + '\t' + '\n'
+len_space_punct = len(space_punct_list)
+
+
 def is_latin(s: str) -> bool:
-    return (65 <= ord(s) < 91) or (97 <= ord(s) < 123)
+    return (ord('A') <= ord(s) < ord('A') + 26) or (ord('a') <= ord(s) < ord('a') + 26)
+
+
+def is_cyrillic(s: str) -> bool:
+    return ord('А') <= ord(s) <= ord('я')
 
 
 def word_to_keyword(s: str, key: str) -> str:
@@ -23,12 +68,12 @@ def word_to_keyword(s: str, key: str) -> str:
     i = 0
     r = 0
     while i < s_len:
-        while i < s_len and not is_latin(s[i]):
+        while i < s_len and not is_latin(s[i]) and not is_cyrillic(s[i]) and s[i] not in space_punct_list:
             s_ret += s[i]
             i += 1
 
         w_len = 0
-        while i < s_len and is_latin(s[i]):
+        while i < s_len and (is_latin(s[i]) or is_cyrillic(s[i]) or s[i] in space_punct_list):
             w_len += 1
             i += 1
 
@@ -54,10 +99,18 @@ def encode_vigenere(s: str, key: str) -> str:
         if is_latin(s[i]):
             if s[i].isupper():
                 k = keyed[i].upper()
-                index = (ord(s[i]) + ord(k) - 2 * 65) % 26 + 65
+                index = (ord(s[i]) + ord(k) - 2 * ord('A')) % 26 + ord('A')
             else:
                 k = keyed[i].lower()
-                index = (ord(s[i]) + ord(k) - 2 * 97) % 26 + 97
+                index = (ord(s[i]) + ord(k) - 2 * ord('a')) % 26 + ord('a')
+            s_ret += chr(index)
+        elif is_cyrillic(s[i]):
+            if s[i].isupper():
+                k = keyed[i].upper()
+                index = (ord(s[i]) + ord(k) - 2 * ord('А')) % 32 + ord('А')
+            else:
+                k = keyed[i].lower()
+                index = (ord(s[i]) + ord(k) - 2 * ord('а')) % 32 + ord('а')
             s_ret += chr(index)
         else:
             s_ret += s[i]
@@ -70,61 +123,51 @@ def encode_caesar(s: str, key: str) -> str:
     key = int(key)
     i = 0
     while i < s_len:
-        if not is_latin(s[i]):
+
+        if not is_latin(s[i]) and not is_cyrillic(s[i]) and s[i] not in space_punct_list:
             s_new += s[i]
-        else:
+            i += 1
+            continue
+        if is_latin(s[i]):
             if s[i].isupper():
-                index = (ord(s[i]) + key - 65) % 26
-                s_new += chr(65 + index)
+                index = (ord(s[i]) + key - ord('A')) % 26
+                s_new += chr(ord('A') + index)
             else:
-                index = (ord(s[i]) + key - 97) % 26
-                s_new += chr(97 + index)
+                index = (ord(s[i]) + key - ord('a')) % 26
+                s_new += chr(ord('a') + index)
+        elif is_cyrillic(s[i]):
+            if s[i].isupper():
+                index = (ord(s[i]) + key - ord('А')) % 32
+                s_new += chr(ord('А') + index)
+            else:
+                index = (ord(s[i]) + key - ord('а')) % 32
+                s_new += chr(ord('а') + index)
+        elif s[i] in space_punct_list:
+            s_new += space_punct_list[(key + space_punct_list.index(s[i])) % len_space_punct]
         i += 1
     return s_new
-
-
-def code_to_keyword(s: str, key: str) -> str:
-    s_ret = ''
-    s_len = len(s)
-    k_len = len(key)
-    i = 0
-    r = 0
-    while i < s_len:
-        while i < s_len and not is_latin(s[i]):
-            s_ret += s[i]
-            i += 1
-
-        w_len = 0
-        while i < s_len and is_latin(s[i]):
-            w_len += 1
-            i += 1
-
-        delta = k_len - r
-        if delta > w_len:
-            s_ret += key[r:w_len + r]
-            r = w_len + r
-            continue
-        #  else:
-        s_ret += key[r:k_len]
-        q = (w_len - delta) // k_len
-        r = (w_len - delta) % k_len
-        s_ret += key * q + key[0:r]
-
-    return s_ret
 
 
 def decode_vigenere(s: str, key: str) -> str:
     s_len = len(s)
     s_ret = ''
-    keyed = code_to_keyword(s, key)
+    keyed = word_to_keyword(s, key)
     for i in range(s_len):
         if is_latin(s[i]):
             if s[i].isupper():
                 k = keyed[i].upper()
-                index = (ord(s[i]) - ord(k)) % 26 + 65
+                index = (ord(s[i]) - ord(k)) % 26 + ord('A')
             else:
                 k = keyed[i].lower()
-                index = (ord(s[i]) - ord(k)) % 26 + 97
+                index = (ord(s[i]) - ord(k)) % 26 + ord('a')
+            s_ret += chr(index)
+        elif is_cyrillic(s[i]):
+            if s[i].isupper():
+                k = keyed[i].upper()
+                index = (ord(s[i]) - ord(k)) % 32 + ord('А')
+            else:
+                k = keyed[i].lower()
+                index = (ord(s[i]) - ord(k)) % 32 + ord('а')
             s_ret += chr(index)
         else:
             s_ret += s[i]
@@ -137,142 +180,282 @@ def decode_caesar(s: str, key: str) -> str:
     s_new = ''
     i = 0
     while i < s_len:
-        if not is_latin(s[i]):
-            s_new += s[i]
-        else:
+        if is_latin(s[i]):
             if s[i].isupper():
-                index = (ord(s[i]) - key - 65) % 26
-                s_new += chr(65 + index)
+                index = (ord(s[i]) - key - ord('A')) % 26
+                s_new += chr(ord('A') + index)
             else:
-                index = (ord(s[i]) - key - 97) % 26
-                s_new += chr(97 + index)
+                index = (ord(s[i]) - key - ord('a')) % 26
+                s_new += chr(ord('a') + index)
+        elif is_cyrillic(s[i]):
+            if s[i].isupper():
+                index = (ord(s[i]) - key - ord('А')) % 32
+                s_new += chr(ord('А') + index)
+            else:
+                index = (ord(s[i]) - key - ord('а')) % 32
+                s_new += chr(ord('а') + index)
+        elif s[i] in space_punct_list:
+            s_new += space_punct_list[(space_punct_list.index(s[i]) - key) % len_space_punct]
+        else:
+            s_new += s[i]
         i += 1
     return s_new
 
 
 def read_info() -> str:
     if args.input_file:
-        with open(f"{args.input_file}", mode='r') as file:
-            string = file.read()
+        try:
+            with open(f"{args.input_file}", mode='r') as file:
+                string = file.read()
+        except OSError:
+            print("error: could not open/read file")
     else:
         string = input()
     return string
 
 
-def write_info(string: str) -> str:
-    if not args.output_file:
-        print(string)
+def write_info(string: str):
+    if args.output_file:
+        try:
+            with open(f"{args.output_file}", mode='w') as file:
+                file.write(string)
+        except OSError:
+            print(f"error: could not open file {file} to write")
     else:
-        with open(f"{args.output_file}", mode='w') as file:
-            file.write(string)
-    return string
+        print(string)
+    return
 
 
+# returns list of all possible strings
 def find_caesar(s: str, _each_letter: list) -> list:
     s_len = len(s)
     s_new_list = []
-    for key in range(26):
-        s_new_list.append('')
-        i = 0
-        while i < s_len:
-            if not is_latin(s[i]):
-                s_new_list[key] += s[i]
-            else:
-                if s[i].isupper():
-                    index = (ord(s[i]) - key - 65) % 26
-                    s_new_list[key] += chr(65 + index)
+    j = 0
+    while j < len(s) and not is_latin(s[j]) and not is_cyrillic(s[j]):
+        j += 1
+    if is_latin(s[j]):
+        for key in range(26):
+            s_new_list.append('')
+            i = 0
+            while i < s_len:
+                if is_latin(s[i]):
+                    if s[i].isupper():
+                        index = (ord(s[i]) - key - ord('A')) % 26
+                        s_new_list[key] += chr(ord('A') + index)
+                    else:
+                        index = (ord(s[i]) - key - ord('a')) % 26
+                        s_new_list[key] += chr(ord('a') + index)
+                    _each_letter[key][index] += 1
+                elif s[i] in space_punct_list:
+                    s_new_list[key] += space_punct_list[(space_punct_list.index(s[i]) - key) % len_space_punct]
                 else:
-                    index = (ord(s[i]) - key - 97) % 26
-                    s_new_list[key] += chr(97 + index)
-                _each_letter[key][index] += 1
-            i += 1
+                    s_new_list[key] += s[i]
+                i += 1
+    elif is_cyrillic(s[j]):
+        for key in range(32):
+            s_new_list.append('')
+            i = 0
+            while i < s_len:
+                if is_cyrillic(s[i]):
+                    if s[i].isupper():
+                        index = (ord(s[i]) - key - ord('А')) % 32
+                        s_new_list[key] += chr(ord('А') + index)
+                    else:
+                        index = (ord(s[i]) - key - ord('а')) % 32
+                        s_new_list[key] += chr(ord('а') + index)
+                    _each_letter[key][index] += 1
+                elif s[i] in space_punct_list:
+                    s_new_list[key] += space_punct_list[(space_punct_list.index(s[i]) - key) % len_space_punct]
+                else:
+                    s_new_list[key] += s[i]
+                i += 1
     return s_new_list
-# returns list of all possible strings
 
 
-def compare_exactness(str_list: list, _each_letter: list, _etalon: list) -> str:
+# takes find_caesar(s) and returns the best match
+def compare_exactness(str_list: list, _each_letter: list, _etalon: list, alphabet_length: int) -> str:
     total = []
     best = 0
-    for i in range(26):
+    if alphabet_length == 0:
+        return 'error: your text does not contain any letters'
+    for i in range(alphabet_length):
         total.append(0)
-        for j in range(26):
+        for j in range(alphabet_length):
             total[i] += (_each_letter[i][j] - float(_etalon[j])) ** 2
-    for i in range(26):
+    for i in range(alphabet_length):
         if total[i] < total[best]:
             best = i
     return str_list[best]
-# takes find_caesar(s) and returns the best match
 
 
-def count_percentage(letters: list, _all: int) -> list:
+def count_percentage(letters: list, _all: int, alphabet_length: int) -> list:
     if _all != 0:
-        for i in range(26):
-            for j in range(26):
+        for i in range(alphabet_length):
+            for j in range(alphabet_length):
                 letters[i][j] /= _all  # _all == len(s)
     return letters
 
 
-if args.part == 'encode':
-    s = read_info()
-    if args.cipher == 'caesar':
-        s_new = encode_caesar(s, args.key)
+# generates random key for Vernam cipher
+def generate_key_vernam(s: str) -> str:
+    key = ''
+    for i in s:
+        if i in baudot_code.keys():
+            key += get_key(random.randint(0, 32))
+    return key
 
-    else:  # if args.cipher == 'vigenere':
-        s_new = encode_vigenere(s, args.key)
-    write_info(s_new)
 
-elif args.part == 'decode':
-    s = read_info()
-    if args.cipher == 'caesar':
-        s_new = decode_caesar(s, args.key)
-
-    else:  # if cipher == 'vigenere':
-        s_new = decode_vigenere(s, args.key)
-    write_info(s_new)
-
-elif args.part == 'train':
-
-    if args.text_file:
-        with open(f"{args.text_file}", mode='r') as file:
-            s = file.read()
-    else:
-        s = input()
+# checks if the Vernam key has the same length as the text
+# and if the key consists of only Baudot symbols
+def check_key_correctness(s: str, key: str) -> bool:
     total = 0
-
-    __each_letter = []
-    for i in range(26):
-        __each_letter += [0]
-    Len = len(s)
-    i = 0
-    while i < Len:
-        if is_latin(s[i]):
-            if s[i].isupper():
-                __each_letter[ord(s[i]) - 65] += 1
-            else:
-                __each_letter[ord(s[i]) - 97] += 1
+    for i in key:
+        if i not in baudot_code.keys():
+            return False
+    for i in s:
+        if i in baudot_code.keys():
             total += 1
+    if total == len(key):
+        return True
+    return False
+
+
+def get_key(val: int) -> str:
+    for key, value in baudot_code.items():
+        if value == val:
+            return key
+
+
+def encode_decode_vernam(s: str, key: str) -> str:
+    s_new = ''
+    i = 0
+    i_key = 0
+    while i < len(s):
+        while i < len(s) and s[i] not in baudot_code.keys():
+            s_new += s[i]
+            i += 1
+        if i == len(s):
+            break
+        if s[i].isupper():
+            s_new += chr(ord('A') - ord('a') +
+                         ord(str(get_key(baudot_code[s[i].lower()] ^ baudot_code[key[i_key].lower()]))))
+        else:
+            s_new += str(get_key(baudot_code[s[i]] ^ baudot_code[key[i_key].lower()]))
         i += 1
-    _etalon = ''
-    for i in range(26):
-        _etalon += str(__each_letter[i] / total)
-        _etalon += ' '
-    with open(f"{args.model_file}", 'w') as file:
-        file.write(_etalon)
+        i_key += 1
+    return s_new
 
-elif args.part == 'hack':
-    with open(f'{args.model_file}', 'r') as file:
-        info = file.read()
 
-    etalon = info.split()
+def main():
+    if args.part == 'encode':
+        s = read_info()
+        s_new = ''
+        if args.cipher == 'caesar':
+            s_new = encode_caesar(s, args.key)
 
-    each_letter = []
-    for i in range(26):
-        each_letter.append([])
-        for j in range(26):
-            each_letter[i].append(0)
+        elif args.cipher == 'vigenere':
+            s_new = encode_vigenere(s, args.key)
 
-    s = read_info()
-    each_letter = count_percentage(each_letter, len(s))
-    all_strs = find_caesar(s, each_letter)
-    best_str = compare_exactness(all_strs, each_letter, etalon)
-    write_info(best_str)
+        elif args.cipher == 'vernam':
+            if args.key:
+                if check_key_correctness(s, args.key):
+                    s_new = encode_decode_vernam(s, args.key)
+                else:
+                    print('error: the key should be of the length of the text')
+            else:
+                key = generate_key_vernam(s)
+                s_new = encode_decode_vernam(s, key)
+                print(f'The randomly generated key is {key}')
+
+        else:
+            print("error: incorrect cipher")
+        write_info(s_new)
+
+    elif args.part == 'decode':
+        s = read_info()
+        s_new = ''
+        if args.cipher == 'caesar':
+            s_new = decode_caesar(s, args.key)
+
+        elif args.cipher == 'vigenere':
+            s_new = decode_vigenere(s, args.key)
+
+        elif args.cipher == 'vernam':
+            if not args.key:
+                print("error: can't hack Vernam cipher")
+                return
+            s_new = encode_decode_vernam(s, args.key)
+
+        else:
+            print("error: incorrect cipher")
+        write_info(s_new)
+
+    elif args.part == 'train':
+        if args.text_file:
+            try:
+                with open(f"{args.text_file}", mode='r') as file:
+                    s = file.read()
+            except OSError:
+                print(f"error: could not open/read file {args.text_file}")
+        else:
+            s = input()
+
+        j = 0
+        while j < len(s) and not is_cyrillic(s[j]) and not is_latin(s[j]):
+            j += 1
+        alphabet_length = 32 * is_cyrillic(s[j]) + 26 * is_latin(s[j])
+        total = 0
+        _each_letter = [0] * alphabet_length
+        s_len = len(s)
+        i = 0
+        while i < s_len:
+            if is_latin(s[i]):
+                if s[i].isupper():
+                    _each_letter[ord(s[i]) - ord('A')] += 1
+                else:
+                    _each_letter[ord(s[i]) - ord('a')] += 1
+                total += 1
+            elif is_cyrillic(s[i]):
+                if s[i].isupper():
+                    _each_letter[ord(s[i]) - ord('А')] += 1
+                else:
+                    _each_letter[ord(s[i]) - ord('а')] += 1
+                total += 1
+            i += 1
+        if total > 0:
+            _etalon = ''
+            for i in range(alphabet_length):
+                _etalon += str(_each_letter[i] / total)
+                _etalon += ' '
+            with open(f"{args.model_file}", 'w') as file:
+                file.write(_etalon)
+        else:
+            print("error: no latin and cyrillic letters found")
+
+    elif args.part == 'hack':
+        try:
+            with open(f'{args.model_file}', 'r') as file:
+                info = file.read()
+        except OSError:
+            print(f"error: could not open/read {file}")
+
+        etalon = info.split()
+        s = read_info()
+        j = 0
+        while j < len(s) and not is_cyrillic(s[j]) and not is_latin(s[j]):
+            j += 1
+        alphabet_length = 0
+        if is_latin(s[j]):
+            alphabet_length = 26
+        elif is_cyrillic(s[j]):
+            alphabet_length = 32
+        each_letter = [[0] * alphabet_length for _ in range(alphabet_length)]
+        each_letter = count_percentage(each_letter, len(s), alphabet_length)
+        all_strs = find_caesar(s, each_letter)
+        best_str = compare_exactness(all_strs, each_letter, etalon, alphabet_length)
+        write_info(best_str)
+
+
+if __name__ == '__main__':
+    main()
+
